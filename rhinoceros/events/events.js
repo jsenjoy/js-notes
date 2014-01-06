f51380b96dc107b8a22339d020f3883e7737b357
Boom <shidhappy@qq.com>
2014-01-06 22:17:09 +0800

    未完成的events练习代码

---
+++ rhinoceros/events/events.js
@@ -0,0 +1,52 @@
+// 这里简单编写一个兼容浏览器的事件绑定工具
+
+!function(window){
+
+    // 事件队列
+    var EventQueue = function(){
+	var _events = [];
+
+	return {
+	    push: function(event){
+		_events.push(event);
+	    },
+	    emit: function(elem){
+		var e = _events, len = e.length, i = 0, args = Array.prototype.slice.call(arguments, 1);
+		
+		for(i; i < len; i++){
+		    if(_events[i].apply(elem, args) === false){
+			return ;
+		    }
+		}	
+	    }
+	};
+    };
+
+    var eventsList = [], uid = 1;
+    
+    // 使用设置元素事件属性的方法来绑定
+    var eventOn = function(elem, type, callback){
+	var id;
+	    
+	elem.__id__ || (elem.__id__ = uid++);
+
+	id = elem.__id__ + ':' + type;
+	eventsList[id] || (eventsList[id] = EventQueue());
+	eventsList[id].push(callback);
+	
+	elem['on' + type] || (elem['on' + type] = function(event){
+	    var event = event || window.event;
+	    eventsList[id]['emit'](elem, event);
+	});
+	
+    };
+
+    // 使用addEventListener方法来绑定
+    var eventBind = function(){
+	
+    };
+
+    window.eventOn = eventOn;
+    window.eventBind = eventBind;
+    
+}(window);
